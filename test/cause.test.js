4977d29a879b7e738e2106c855e1283f42ad88a1
Brian Besaw <bjbesaw@outlook.com>
2017-12-20 20:00:30 -0500

    Updated paths to .env in test files.

--- test/cause.test.js
+++ test/cause.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const path = require('path');
-const dotEnvPath = path.resolve('../../.env');
+const dotEnvPath = path.resolve('../.env');
 require('dotenv').config({path: dotEnvPath});
 
 const { setDbMode } = require('../config');
